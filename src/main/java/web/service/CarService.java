fb5eed9a833642d30dbe35aa6cefed1a890811c0
Vladislav <random6@bk.ru>
2022-03-30 16:03:45 +0300

    Remove the generic out of the CarService method

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,13 +1,14 @@
 package web.service;
 
 import org.springframework.stereotype.Service;
+import web.model.Car;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class CarService {
-    public <T> List<T> getSubList(List<T> list, int count) {
+    public List<Car> getSubList(List<Car> list, int count) {
         return list.stream().limit(count > 0 ? Math.min(count, 5) : 5).collect(Collectors.toList());
     }
 }
